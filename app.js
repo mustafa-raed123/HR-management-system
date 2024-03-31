b5bfe9809098e3e9fd52f34e632a27a3baa4cd3e
mustafa-raed123 <mwsym1898@gmail.com>
2024-03-31 12:49:50 +0000

    d

--- app.js
+++ app.js
@@ -7,56 +7,75 @@ function Employee(ID,Name,Department,Level,Image){
     this.salary =0;
     this.netsalary = 0;
 } 
-let employees=[
-new Employee(1000,"Ghazi Samer","Administration","Senior","https://as1.ftcdn.net/v2/jpg/02/43/12/34/1000_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"),
-new Employee(1001,"Lana Ali","Finance","Senior","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSQKaS7LP80SEcKgz9-d_ORjkh1B9hPSUqkeI_mLSnDg&s"),
-new Employee(1002,"Tamara Ayoub","Marketing","Senior","https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1711632238~exp=1711632838~hmac=fa2fd6fa508aa262860545d89e34d394eacbf5fe04f8b3003c031723f6ce2032"),
-new Employee(1003,"Safi Walid","Administration","Mid-Senior","https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1711632238~exp=1711632838~hmac=fa2fd6fa508aa262860545d89e34d394eacbf5fe04f8b3003c031723f6ce2032"),
-new Employee(1004,"Omar Zaid","Development","Senior","https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1711632238~exp=1711632838~hmac=fa2fd6fa508aa262860545d89e34d394eacbf5fe04f8b3003c031723f6ce2032"),
-new Employee(1005,"Rana Saleh","Development","Junior","https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1711632238~exp=1711632838~hmac=fa2fd6fa508aa262860545d89e34d394eacbf5fe04f8b3003c031723f6ce2032"),
-new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=996&t=st=1711632238~exp=1711632838~hmac=fa2fd6fa508aa262860545d89e34d394eacbf5fe04f8b3003c031723f6ce2032"),
-];
 Employee.prototype.calaculatesalary = function(){
     const salary = {
-        "Junior" :    { min: 30000, max: 50000 },
+        "Junior" :     { min: 30000, max: 50000 },
         "Mid-Senior" : { min: 50000, max: 80000 },
-        "Senior" : { min: 80000, max: 120000 }
+        "Senior" :     { min: 80000, max: 120000 }
     };
-    const salaryRange = salary [this.level] ;
+    const salaryRange = salary[this.level] ;
     this.salary = Math.floor(Math.random() * (salaryRange.max - salaryRange.min + 1)) + salaryRange.min;
 }
 Employee.prototype.calaculatenetsalary = function(){
     let percent = 7.5;
-     this.netsalary = Math.floor(this.salary - ((this.salary * percent) / 100));   
+    this.netsalary = Math.floor(this.salary - ((this.salary * percent) / 100));   
  }
- Employee.prototype.render = function(){
-     let main = document.querySelector(".main")
-     let div = document.createElement("div")
-     let p = document.createElement("p");
-     let sal = document.createElement("p")
-     p.innerHTML = this.name;
-     p.style.fontSize = "20px";
-     p.style.fontWeight ="bold"
-     sal.innerHTML =` salary: ${this.salary}   netSalaty:  ${this.netsalary} `
-     sal.style.paddingTop ="10px";
-     sal.style.fontWeight ="bold"
-     sal.style.color="#fff"
-     sal.style.fontSize = "19px"; 
-     div.classList.add("empname");
-     div.append(p)
-     div.append(sal)    
-     div.style.padding= "10px";
-     div.style.marginTop= "10px";
-     div.style.backgroundColor = "#00aa88";
-     main.appendChild(div)
-     console.log(div) 
-}
-employees.forEach((e)=>{
-   console.log(e.calaculatesalary())
-   console.log(e.calaculatenetsalary())
+ function uniqueId(count){
+    return 1000 + count;
+   }  
+   let employees = [];
+   let count = 0;
+   document.querySelector("form").addEventListener("submit",(event)=>{
+    event.preventDefault();
+  
+
+  let name = event.target.names.value;
+  
+  let department = event.target.Department.value;
+
+  let level = event.target.level.value;
+  let img = event.target.image.value
+  console.log(name , department , level , img)
+
+console.log(employees[0])
+
+  if(!(name == "" ||department == "Enter your Department" || level =="Enter your level" || img == "")){
+   employees.push(new Employee(parseInt(uniqueId(count  + 1 )),name,department,level,img));  
+   console.log(employees)
+   employees[count].calaculatesalary();
+   employees[count].calaculatenetsalary();
+   employees[count].render();
+   count++;
+    
+
+
+
+   
+  }else{
+    alert("Enter correct information");
+  }
+    
+
 })
 
-employees.forEach((e)=>{
- e.render()
- })
 
+
+Employee.prototype.render= function() {
+    let sec=document.querySelector(`.${this.department}`)
+    console.log(sec)
+    sec.innerHTML += `<div class="box" style="width: 300px; height: 400px;  border-radius: 5px; border: 1px solid #444; background-color: #00aa88; text-align: center; padding: 20px; color: #fff; "> 
+    <div class="img" style="background-size: cover; text-align: center;">
+    <img src="image/${this.img}.jpg" alt="" style="background-size: cover;width: 200px; height: 200px;">
+    </div>   
+    <p style="color: #fff; margin-bottom: 10px; margin-top: 10px; ">Name: ${this.name}- <span>Id: ${this.id}</span></p>        
+    <p>department: ${this.department} - <span>${this.level}</span></p>
+    <p>salary: ${this.salary} </p>
+     <span>net salary ${this.netsalary}</span>
+    </div>`
+
+    document.querySelector("main form #name").value = "";
+    document.querySelector("main form #image").value = "";
+       document.querySelector("#select_debartment").value =["Enter your Department"];
+       document.querySelector("#select_level").value =["Enter your level"];
+
+    }
